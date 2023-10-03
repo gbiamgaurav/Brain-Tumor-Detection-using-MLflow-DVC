11d57e7dd75d921fe78f211f94f09aed37524936
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-10-03 13:19:06 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from brain_tumor import logger
 from brain_tumor.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
+from brain_tumor.pipeline.stage_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -10,4 +11,15 @@ try:
     logger.info(f"Stage: {STAGE_NAME} Completed!")
 except Exception as e:
     logger.exception(e)
-    raise e 
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f"Stage: {STAGE_NAME} Started!")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f"Stage: {STAGE_NAME} Completed!")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/brain_tumor/config/configuration.py
+++ src/brain_tumor/config/configuration.py
@@ -1,7 +1,8 @@
 
 from brain_tumor.constants import * 
 from brain_tumor.utils.main_utils import read_yaml, create_directories
-from brain_tumor.entity.config_entity import DataIngestionConfig
+from brain_tumor.entity.config_entity import (DataIngestionConfig, 
+                                              PrepareBaseModelConfig,)
 from pathlib import Path
 
 
@@ -29,3 +30,21 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model 
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
