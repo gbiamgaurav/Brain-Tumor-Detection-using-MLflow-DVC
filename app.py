63f209433d6f9877815fa5cedc235f3769e47bbc
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-10-06 14:14:45 +0530

    changed

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 import streamlit as st
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from brain_tumor.pipeline.prediction import braindata
+from tensorflow.keras.models import load_model
+from brain_tumor.pipeline import prediction
 
 # Set Streamlit page title and favicon
 st.set_page_config(
-    page_title="Brain Tumor Image Classifier Webapp",
-    page_icon="🧠",
+    page_title="Brain Tumor Image Classification WebApp",
+    page_icon="🥭",
 )
 
+# Load the pre-trained model
+model = load_model("final_model/brain_model.h5")
+
+# Define class labels
+class_labels = {
+    0: "giloma",
+    1: "meningioma",
+    2: "notumor",
+    3: "pituitary",
+
+}
+
 # Define app title and header
-st.title("Brain Tumor Image Classification")
-st.write("Upload an image of a brain to classify if it has a Tumor or Not")
+st.title("Brain Tumor Image Classifier")
+st.write("Upload an image of a brain to classify if it has a tumor or not")
 
 # Create a file uploader widget
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
@@ -24,14 +38,24 @@ if uploaded_image is not None:
     # Display the uploaded image
     st.image(uploaded_image, caption="Uploaded Brain Image", use_column_width=True)
 
-    # Create an instance of the braindata class
-    predictor = braindata(filename=uploaded_image)
+    # Preprocess the image
+    img = image.load_img(uploaded_image, target_size=(180, 180))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
 
-    # Make a prediction using the predictor
-    prediction_result_data = predictor.predictbraindata()
+    # Make a prediction
+    result = model.predict(img_array)
+    class_index = np.argmax(result, axis=1)
 
-    # Display the prediction result in Streamlit
-    st.write("Prediction:", prediction_result_data[0]['image'])
+    # Determine whether the result is Healthy or Disease
+    prediction = "Tumor" if class_index[0] == 5 else "No Tumor"
+
+    # Display the prediction result in bold
+    prediction_result.markdown(f"**Prediction: {prediction}**")
 
 # Add some space and a footer
 st.write("\n\n")
+st.markdown(
+    "Made with ❤️ by Gaurav Bhattacharya\n"
+    
+)
